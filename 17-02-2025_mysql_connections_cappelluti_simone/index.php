531caaec873caae15164a5ff4d3e65b3efa808ee
MinimumADHD <91323541+MinimumADHD@users.noreply.github.com>
2025-02-19 13:24:04 +0100

    *

--- 17-02-2025_mysql_connections_cappelluti_simone/index.php
+++ 17-02-2025_mysql_connections_cappelluti_simone/index.php
@@ -1,58 +1,37 @@
 <!DOCTYPE html>
-<html lang="en" class="w-full h-full min-h-full min-w-full bg-slate-700 font-fira text-[13px] text-neutral-50">
+<html lang="en" class="min-w-full min-h-full w-full h-full">
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>MYSQL + PHP</title>
+    <title>Document</title>
     <link rel="stylesheet" href="../src/output.css">
-    <link rel="shortcut icon" href="../favicon.ico" type="image/x-icon">
 </head>
 
-<body class="w-full h-full min-h-full min-w-full">
-    <!DOCTYPE html>
-    <html lang="en">
+<body class="w-full h-full bg-amber-600">
+    <table class="w-auto h-auto">
+        <?php
+        $host_name = (string) "localhost";
+        $user_name = (string) "root";
+        $pass_word = (string) "";
+        $data_base_name = (string) "utenze";
 
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Document</title>
-    </head>
+        $connection = mysqli_connect($host_name, $user_name, $pass_word, $data_base_name);
+        // $errno_connection = mysqli_errno($connection);
+        // echo"{$errno_connection}";
+        if (mysqli_connect_errno() && mysqli_errno($connection) != 0)
+            return;
+        $table_query = mysqli_query($connection, "SELECT * FROM utente;");
 
-    <body>
-        <header class="w-full h-[6%] bg-slate-600 flex justify-center items-center align-middle p-2 gap-3">
-            <h1 class="font-extrabold text-[15px]">Connessioni PHP-Database</h1>
-            <h1><a href="../">Pagina Principale</a></h1>
-        </header>
-        <div class="w-full h-0.5 bg-neutral-50"></div>
-        <main class="w-full h-full flex justify-center align-middle items-center">
-            <table class="w-auto h-auto m-3">
-                <?php
-                $host_name = (string) "localhost";
-                $user_name = (string) "root";
-                $pass_word = (string) "";
-                $data_base_name = (string) "utenze";
-
-                $connection = mysqli_connect($host_name, $user_name, $pass_word, $data_base_name);
-                // $errno_connection = mysqli_errno($connection);
-                // echo"{$errno_connection}";
-                if (mysqli_connect_errno() && mysqli_errno($connection) != 0)
-                    return;
-                $table_query = mysqli_query($connection, "SELECT * FROM utente;");
-
-                foreach ($table_query as $i => $value) {
-                    echo"<tr class='border-2 gap-2 p-2'>";
-                    foreach ($value as $j => $row) {
-                        echo "<td class='m-2 p-2 border-2 align-middle items-center'>{$row}</td>";
-                    }
-                    echo"</tr>";
-                }
-                ?>
-            </table>
-        </main>
-    </body>
-
-    </html>
+        foreach ($table_query as $row) {
+            echo "<tr class='border-2 gap-2 p-2'>";
+            foreach ($row as $column_name => $column_value) {
+                echo "<td class='m-2 p-2 border-2 align-middle items-center'>{$column_value}</td>";
+            }
+            echo "</tr>";
+        }
+        ?>
+    </table>
 </body>
 
 </html>
